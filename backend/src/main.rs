a513cebc239b8e016ee41d706a9908ff514d99d4
Kristian Mika <kristian.mika@protonmail.com>
2023-03-01 22:07:07 +0100

    feat: serve static files in backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,25 @@
 mod endpoints;
 mod http_response;
+use log::info;
 use std::io;
 
 use actix_web::{App, HttpServer};
 use endpoints::measurement;
 
 static LISTENING_ADDRESS: &str = "0.0.0.0:8080";
+static WEB_FILES_PATH: &str = "/var/www/pv191-smart-home-server/";
+static INDEX_FILE: &str = "index.html";
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| App::new().service(measurement))
-        .bind(LISTENING_ADDRESS)?
-        .run()
-        .await
+    env_logger::init();
+    info!("Starting server on address {}.", LISTENING_ADDRESS);
+    HttpServer::new(|| {
+        App::new()
+            .service(measurement)
+            .service(actix_files::Files::new("/", WEB_FILES_PATH).index_file(INDEX_FILE))
+    })
+    .bind(LISTENING_ADDRESS)?
+    .run()
+    .await
 }
