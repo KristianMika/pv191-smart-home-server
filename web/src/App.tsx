661324649d8a6c972fad9dd61a5842e8616a4d7d
Kristian Mika <kristian.mika@protonmail.com>
2023-03-26 21:14:24 +0200

    feat: notify the user of high VOC index

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Home } from "./components/Home/Home";
 
 function App() {
+  Notification.requestPermission();
   return (
     <div className="App">
       <Home />
--- web/src/components/Home/Home.tsx
+++ web/src/components/Home/Home.tsx
@@ -8,10 +8,29 @@ import { MeasurementTime } from "./CurrentMeasurements/MeasurementTime";
 import { MeasurementHistory } from "./HistoryMeasurements/MeasurementHistory";
 
 const REFRESH_INTERVAL_MS = 10 * 1000;
+const VOC_INDEX_NOTIFICATION_THRESHOLD = 60;
+const TEN_MINUTES = 1000 * 60 * 10;
+
 export const Home: React.FC = () => {
+  const checkVoc = (vocIndex?: number) => {
+    if (
+      vocIndex &&
+      vocIndex > VOC_INDEX_NOTIFICATION_THRESHOLD &&
+      lastNotificationTime > Date.now() - TEN_MINUTES
+    ) {
+      setLastNotificationTime(Date.now());
+      new Notification("High VOC alert", {
+        body: `We have detected VOC of ${vocIndex}!`,
+        icon: "logo192.png",
+      });
+    }
+  };
   const username = "Andy"; //TODO: load from JWT
   const [multiMeasurement, setMultiMeasurement] = useState<Measurement[]>([]);
   const [measurementTime, setMeasurementTime] = useState<Date>();
+  const [lastNotificationTime, setLastNotificationTime] = useState<number>(
+    Date.now()
+  );
   useEffect(() => {
     axios.get("/api/measurement").then((response) => {
       const data: IMeasurementResponse = response.data as IMeasurementResponse;
@@ -21,6 +40,7 @@ export const Home: React.FC = () => {
         { type: MeasurementType.Humidity, value: data.humidity },
         { type: MeasurementType.Voc, value: data.voc_index },
       ];
+      checkVoc(data.voc_index);
       setMeasurementTime(new Date(data.measurement_time));
       setMultiMeasurement(measurements);
     });
