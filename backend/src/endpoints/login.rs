9646c674fff67630eb853899ee583b9619ad702f
Kristian Mika <kristian.mika@protonmail.com>
2023-05-14 12:26:04 +0200

    feat: set jwt dummy variable
    set access and refresh cookies as httpOnly

--- backend/src/endpoints/login.rs
+++ backend/src/endpoints/login.rs
@@ -5,7 +5,7 @@ use crate::{
     state::ServerState,
 };
 use actix_jwt_auth_middleware::{AuthResult, TokenSigner};
-use actix_web::{post, web, HttpResponse};
+use actix_web::{cookie::Cookie, post, web, HttpResponse};
 use jwt_compact::alg::Ed25519;
 use log::error;
 
@@ -46,8 +46,25 @@ pub(crate) async fn post_login(
         }));
     }
     let user = User::default();
+
+    let mut access_cookie = token_signer.create_access_cookie(&user)?;
+    let mut refresh_cookie = token_signer.create_refresh_cookie(&user)?;
+    let dummy_jwt_indicator_cookie = create_jwt_indicator_cookie();
+    access_cookie.set_http_only(true);
+    refresh_cookie.set_http_only(true);
+
     Ok(HttpResponse::Ok()
-        .cookie(token_signer.create_access_cookie(&user)?)
-        .cookie(token_signer.create_refresh_cookie(&user)?)
+        .cookie(access_cookie)
+        .cookie(refresh_cookie)
+        .cookie(dummy_jwt_indicator_cookie)
         .finish())
+}
+
+/// Creates a jwt indicator cookie that is not-HttpOnly
+///
+/// It's main purpose is to indicate to front-end when
+/// the user is loged-in, as we can't access httpOnly
+/// cookies from JS
+fn create_jwt_indicator_cookie() -> Cookie<'static> {
+    Cookie::new("jwt_set", "dummy value")
 }
