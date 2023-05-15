388d657985b81a9d9b92e05630ffb47baa170542
Kristian Mika <kristian.mika@protonmail.com>
2023-05-15 10:00:17 +0200

    chore: update schema.rs

--- backend/common/src/server_repo/postgres_server_repo/schema.rs
+++ backend/common/src/server_repo/postgres_server_repo/schema.rs
@@ -23,4 +23,7 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(measurement, usercontext,);
+diesel::allow_tables_to_appear_in_same_query!(
+    measurement,
+    usercontext,
+);
